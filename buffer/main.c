2af66ac4a16e4473d58a761b5ed801652c6278dc
oestrangeiro <mateus12092004@gmail.com>
2023-06-15 23:56:42 -0300

    commit

--- buffer/main.c
+++ buffer/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdio_ext.h>
+
 
 int main(void){
+
+	char letra, letra2;
 	
+	printf("insira uma letra do teclado: ");
+	scanf("%c", &letra);
+	__fpurge(stdin);	//Limpando o buffer
+	printf("insira outra letra: ");
+	scanf("%c", &letra2);
+	__fpurge(stdin);
 	
-		
+	printf("as letras digitadas foram: '%c' e '%c'", letra, letra2);
+
 	return 0;
 }
+	
